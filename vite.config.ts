478e5e121b1051a14125ff978536919fc0595280
Shain922 <fmhd48763@gmail.com>
2026-05-16 18:20:32 +0530

    fix: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,4 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   cloudflare: false,
-  tanstackStart: {
-    server: {
-      preset: "vercel",
-    },
-  },
 });
